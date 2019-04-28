3e0e8dcbdb9f46d98583ab2d75fe7177afaae12b
LAPTOP-6LQBHRCU\skyli <skylife927@naver.com>
2019-04-28 17:43:49 +0900

    공유할때 by nugasam추가

--- app/src/main/java/com/moon/nugasam/MainActivity.kt
+++ app/src/main/java/com/moon/nugasam/MainActivity.kt
@@ -74,11 +74,11 @@ class MainActivity : AppCompatActivity() {
         ForceUpdateChecker.with(this).onUpdateNeeded(ForceUpdateChecker.OnUpdateNeededListener {
             Log.d("MQ!", "updateNeedListener $this")
             val dialog = AlertDialog.Builder(this)
-                    .setTitle("강제 업데이트")
-                    .setMessage("업데이트는 필수입니다.")
-                    .setPositiveButton(
-                            "Update"
-                    ) { _, _ -> redirectStore(it) }
+                .setTitle("강제 업데이트")
+                .setMessage("업데이트는 필수입니다.")
+                .setPositiveButton(
+                    "Update"
+                ) { _, _ -> redirectStore(it) }
 //                  .setNegativeButton(
 //                        "No, thanks"
 //                    ) { _, _ -> finish() }.create()
@@ -159,8 +159,8 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.action_profile -> {
                 var intent = Intent(
-                        this@MainActivity,
-                        GoogleSignInActivity::class.java
+                    this@MainActivity,
+                    GoogleSignInActivity::class.java
                 )
                 intent.putExtra("name", me?.name)
                 startActivity(intent)
@@ -206,21 +206,7 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.action_share -> {
-                var shareBody = ""
-                var index = 0
-                for (user in datas) {
-                    if (index == datas.lastIndex) {
-                        shareBody += (user.name + " " + user.nuga)
-                    } else {
-                        shareBody += (user.name + " " + user.nuga + "\n")
-                    }
-                    index++
-                }
-                var intent = Intent(android.content.Intent.ACTION_SEND).apply {
-                    setType("text/plain")
-                    putExtra(android.content.Intent.EXTRA_TEXT, shareBody)
-                }
-                startActivity(Intent.createChooser(intent, System.currentTimeMillis().toString()))
+                share()
                 return true
             }
             R.id.action_cat -> {
@@ -273,47 +259,65 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.action_undo -> {
-                startActivity(Intent(
+                startActivity(
+                    Intent(
                         this@MainActivity,
-                        UndoActivity::class.java))
+                        UndoActivity::class.java
+                    )
+                )
                 return true
             }
             R.id.item_done -> {
                 Log.d(TAG, "done clicked selectionList:$selectionList")
                 SelectDialog.build(
-                        this@MainActivity, "정말 샀나요?", "", "샀음", DialogInterface.OnClickListener { dialog, inputText ->
-                    var ref = FirebaseDatabase.getInstance().getReference()
-                    var undoRef = FirebaseDatabase.getInstance().getReference().child("history").push()
-                    var undoData = UndoData()
-                    undoData.me = me
-                    undoData.date = System.currentTimeMillis().toString()
-                    var who = ArrayList<User>()
-                    val childUpdates = HashMap<String, Any>()
-                    for (user in selectionList) {
-                        var key = getKey(user)
-                        user.nuga += -1
-                        me?.let {
-                            it.nuga += 1
+                    this@MainActivity, "정말 샀나요?", "", "샀음", DialogInterface.OnClickListener { dialog, inputText ->
+                        var ref = FirebaseDatabase.getInstance().getReference()
+                        var undoRef = FirebaseDatabase.getInstance().getReference().child("history").push()
+                        var undoData = UndoData()
+                        undoData.me = me
+                        undoData.date = System.currentTimeMillis().toString()
+                        var who = ArrayList<User>()
+                        val childUpdates = HashMap<String, Any>()
+                        for (user in selectionList) {
+                            var key = getKey(user)
+                            user.nuga += -1
+                            me?.let {
+                                it.nuga += 1
+                            }
+                            Log.d(TAG, "key:$key")
+                            who.add(user)
+                            childUpdates.put("/users/" + key, user)
                         }
-                        Log.d(TAG, "key:$key")
-                        who.add(user)
-                        childUpdates.put("/users/" + key, user)
-                    }
-                    Log.i(TAG, "done me : $me")
-                    me?.let { childUpdates.put("/users/" + getKey(me!!), it) }
-                    Log.i(TAG, "done childUpdates: $childUpdates")
-                    ref.updateChildren(childUpdates)
+                        Log.i(TAG, "done me : $me")
+                        me?.let { childUpdates.put("/users/" + getKey(me!!), it) }
+                        Log.i(TAG, "done childUpdates: $childUpdates")
+                        ref.updateChildren(childUpdates)
 
-                    undoData.who = who
-                    undoRef.setValue(undoData)
+                        undoData.who = who
+                        undoRef.setValue(undoData)
 
-                    clearActionMode()
-                    myAdapter?.notifyDataSetChanged()
-                    dialog.dismiss()
-                }, "취소", DialogInterface.OnClickListener { dialog, which ->
-                    dialog.dismiss()
-                    clearActionMode()
-                }).apply {
+                        clearActionMode()
+                        myAdapter?.notifyDataSetChanged()
+                        dialog.dismiss()
+
+                        SelectDialog.build(
+                            this@MainActivity,
+                            "공유하시겠습니까?",
+                            "",
+                            "공유",
+                            DialogInterface.OnClickListener { dialog, inputText ->
+                                dialog.dismiss()
+                                share()
+                            },
+                            "취소",
+                            DialogInterface.OnClickListener { dialog, which -> dialog.dismiss() }).apply {
+                            setDialogStyle(1)
+                            showDialog()
+                        }
+                    }, "취소", DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+                        clearActionMode()
+                    }).apply {
                     setDialogStyle(1)
                     showDialog()
                 }
@@ -321,6 +325,25 @@ class MainActivity : AppCompatActivity() {
             }
             else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun share() {
+        var shareBody = ""
+        var index = 0
+        for (user in datas) {
+            if (index == datas.lastIndex) {
+                shareBody += (user.name + " " + user.nuga)
+            } else {
+                shareBody += (user.name + " " + user.nuga + "\n")
+            }
+            index++
+        }
+        shareBody += "\nby NUGA3"
+        var intent = Intent(android.content.Intent.ACTION_SEND).apply {
+            setType("text/plain")
+            putExtra(android.content.Intent.EXTRA_TEXT, shareBody)
+        }
+        startActivity(Intent.createChooser(intent, System.currentTimeMillis().toString()))
     }
 
     fun clearActionMode() {
@@ -347,9 +370,9 @@ class MainActivity : AppCompatActivity() {
     fun loadFirebaseData() {
         if (query == null) {
             query =
-                    FirebaseDatabase.getInstance().getReference().child("users").orderByChild(reorder!!).apply {
-                        addValueEventListener(postListener)
-                    }
+                FirebaseDatabase.getInstance().getReference().child("users").orderByChild(reorder!!).apply {
+                    addValueEventListener(postListener)
+                }
         } else {
             query?.removeEventListener(postListener)
             query = FirebaseDatabase.getInstance().getReference().child("users").orderByChild(reorder!!).apply {
